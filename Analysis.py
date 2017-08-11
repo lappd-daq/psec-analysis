0b76e7296fd3e586673e5080978a4ba480e47db8
Miles Lucas <mdlucas@iastate.edu>
2017-08-11 11:18:58 -0500

    Added readme and commented analysis

--- Analysis.py
+++ Analysis.py
@@ -1,24 +1,62 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-plt.style.use('seaborn')
 
-nSamples = 256
-nCols = 33
-
+NUM_SAMPLES = 256
+NUM_COLS = 33
 
 def load_data(filename, num_events):
+    '''
+    Parses the text file for PSEC analysis
+
+    Arguments
+    ---------
+    filename: string
+        the full path to the txt file containing the data
+
+    num_events: int
+        The number of events recorded in the data file. If this is too small, data will not be parsed and if it is too large, the parsing will reach EOF prematurely
+
+    Returns
+    -------
+    data: 3d-array
+        An array with the form [Channel, Event, Sample] containing the event data and metadata. There are 33 channels, the first 2 are timing values. The next 30 are data channels. The last channel is metadata. See form_metadata.cpp in Eric's source code for an explanation of the metadata. There are 'num_events' events. Each event has 256 samples.
+
+    ped: 2d-array
+        An array with the form [Channel, Sample] containing the pedestal data. The channel layout is the same as the data array and there are still 256 samples. Note that the 2nd and 33rd columns of this array are all zeros.
+    '''
     data = np.genfromtxt(filename)
-    ped = data[:nSamples].T
+    ped = data[:NUM_SAMPLES].T
 
     channel_data = [[
-            data[nSamples * (i + 1):(i + 2) * nSamples, ch]
+            data[NUM_SAMPLES * (i + 1):(i + 2) * NUM_SAMPLES, ch]
          for i in range(num_events)
-        ] for ch in range(nCols)]
+        ] for ch in range(NUM_COLS)]
 
     return (np.asarray(channel_data), ped)
 
-def plot_event(data,ch, ped, event=0, path=None):
+def plot_event(data, ped, ch, event=0, path=None):
+    '''
+    Simple plot tool for quick use the data array
+
+    Arguments
+    ---------
+    data: 3d-array
+        The data array from the load_data command. See that documentation for more information
+
+    ped: 2d-array
+        The ped array from the load_data command. See that documentation for more information
+
+    ch: int
+        The channel to plot. Note: this is the actual data channel (eg channel 2 from the psec board would input 2) not the column of the data array.
+
+    event: int, optional
+        The event to plot (Default=0)
+    '''
+
+    plt.style.use('seaborn')
+    plt.figure()
+
     plt.plot(data[ch + 1, event] - ped[ch+1], label='Evt:{}'.format(event))
     plt.legend()
     plt.title('Channel:Event | {}:{}'.format(ch, event))
@@ -26,8 +64,7 @@ def plot_event(data,ch, ped, event=0, path=None):
     plt.ylabel('ADC Counts')
     plt.gca().invert_yaxis()
     plt.show()
-    if path:
-        directory = 'figs/{}'.format(path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        plt.savefig('{}/ch{}-{}'.format(directory, ch, events))
+
+
+if __name__=='__main__':
+    pass
---
+++ README.md
@@ -0,0 +1,34 @@
+# PSEC Analysis
+
+Miles Lucas - Iowa State University
+
+This code serves as a simple parsing tool for the PSEC data created by Dr. Eric Oberla's PSEC acdc-daq.
+
+## Requirements
+
+- Python 3.4+
+- numpy
+- matplotlib
+
+## Data Format
+
+Eric's data output is laid out in a text file with the following format // TODO
+
+## Usage
+
+```
+In [1]: import Analysis as ana
+
+In [2]: data, ped = ana.load_data('data/test.acdc.dat', 50)
+
+In [3]: ccTimeStampLo = data[32, :, 37]
+
+In [4]: print(ccTimeStampLo)
+[ 1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.
+  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.
+  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.
+  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.
+  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.  1617.]
+
+In [5]: plot = ana.plot_event(data, ped, 2, event=25)
+```
